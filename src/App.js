20e54c2777a17f729c7ae1737825e64240743a90
asekawa <40081059+asekawa@users.noreply.github.com>
2021-05-22 20:02:26 +0530

    header changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import './components/shared components/header/header'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/shared components/header/header';
 
 function App() {
 
   return (
+    
     <div className="app">
         <h2>Welcome to panhinda!</h2>
 
@@ -16,7 +18,8 @@ function App() {
       <input type="text" className="form-control item" placeholder="email"/>
       </div>
       <input type="button" className="form-control item" value="submit"/>
-      </div>
+    </div>
+  
     </div>
 
   );
---
+++ src/components/shared components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+const NavBar = () => (
+    <header className='navbar'>
+        <div className='navbar__title navbar__item'>Home</div>
+        <div className='navbar__item'>About Us</div>
+        <div className='navbar__item'>Logout</div>
+        
+    </header>
+);
+
+export default NavBar;
