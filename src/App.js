575f6f588f51eb521175a61647cf57773455bdc7
Nethmee <nssellahewa@gmail.com>
2021-05-22 19:25:27 +0530

    log in component seperated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from "./components/pages/log in/login";
 
 function App() {
 
   return (
-    <div className="app">
-        <h2>Welcome to panhinda!</h2>
-
-      <div className="login-container">
-      <div class="form-group">
-      <input type="text" className="form-control item" placeholder="email"/>
-      </div>
-      <div class="form-group">
-      <input type="text" className="form-control item" placeholder="email"/>
-      </div>
-      <input type="button" className="form-control item" value="submit"/>
-      </div>
+    <div className="app-container">
+       <Login/>
     </div>
 
   );
