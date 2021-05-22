bf17efada6ab5c7ce3035e8ad5e750ea963d60df
Nethmee <nssellahewa@gmail.com>
2021-05-22 20:32:03 +0530

    scss issue solved

--- src/components/pages/dashboard/dashboard.js
+++ src/components/pages/dashboard/dashboard.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './dashboard.scss'
 
 class dashboard extends Component {
     render() {
         return (
-            <div>
+            <div className ="container1">
                 <h5>Welcome to Panhinda!</h5>
             </div>
         );
