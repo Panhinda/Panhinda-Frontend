5954664dc94e765a019d1b27f2c16931b2f67fed
asekawa <40081059+asekawa@users.noreply.github.com>
2021-06-06 15:17:50 +0530

    axios

--- src/components/pages/dashboard/dashboard.js
+++ src/components/pages/dashboard/dashboard.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 import './dashboard.css';
 import Header from '../../shared components/header/header';
 import Createpost from "../create post/createPost";
+import axios from 'axios';
+import { post } from 'jquery';
+
 
 class dashboard extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       posts:[]
+    }
+  }
+   
+  componentDidMount()
+  {
+   /*  axios. get('https://jsonplaceholder.typicode.com/posts')  */
+    axios.get('http://localhost:3000/getAllPosts') 
+    
+    .then(response=>{
+      console.log(response)
+      this.setState({posts:response.data.payload})
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+  }
     render() {
+      const{posts}=this.state// destructuring
         return (
             <div className ="container1">
                 <Header/>
@@ -14,7 +39,30 @@ class dashboard extends Component {
                 <hr/>
                 </div>
                 <div>
-                <table>
+                  list of posts
+                  {
+                    /* posts.length? */
+                    posts.map(post=>
+                      <div key={post.id}>
+                         <table>
+  <tr>
+    <th>Post ID</th>
+    <th>Content</th>
+    <th>Type</th>
+
+  </tr>
+  <tr>
+    <td>{post.id}</td>
+    <td>{post.type}</td>
+    <td> {post.content}</td>
+  
+  </tr>
+  </table>
+                        
+                        
+                        </div>)/* :null */
+                  }
+     {/*            <table>
   <tr>
     <th>Post ID</th>
     <th>Title</th>
@@ -65,7 +113,7 @@ class dashboard extends Component {
     <td></td>
   </tr>
 </table>
-                </div>
+          */}       </div>
 
             </div>
             
