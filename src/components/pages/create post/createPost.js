008cfcac091e949e32a7e5c82e87239efaeab6ca
asekawa <Asekawar@gmail.com>
2021-07-29 01:09:57 +0530

    create post API connect

--- src/components/pages/create post/createPost.js
+++ src/components/pages/create post/createPost.js
@@ -1,42 +1,84 @@
 import React, { Component } from 'react';
 import './createPost.scss'
+import axios from 'axios';
+
 class createPost extends Component {
+
+    // state = {
+    //     user_id: 1,
+    //     title: '',
+    //     content: '',
+    //     type: '',
+    //     author: ''
+    // }
+
+    submit = (event) => {
+        event.preventDefault();
+        const obj = {
+            title: this.title.value,
+            content: this.content.value,
+            type: this.type.value,
+            author: this.author.value,
+            user: this.id.value
+        }
+
+        axios.post('http://localhost:3000/createPost',obj).then((res) => {
+            console.log(res);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
     render() {
         return (
             <div className="container">
-                Create Posts
-                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
-                <div className="card-header "><h1>Create Posts</h1></div>
-                
+
+                    <div className="card-header"><h1>Create Posts</h1></div>
+
                     <div className="card card-statistics custom-card-bottom">
-                        <div className="col-11">
 
-                        <label
-                            htmlFor="Title"
-                            
-                          >
-                            Title:
-                            </label>
-                            <div className="row">
-                            <input
-                              type="text"
-                              name="Title"
-                              className="form-control"
-                              id="formGroupExampleInput"
-                              //onChange={handleChange}
-                              //disabled={true}
-                              //value={}
-                              placeholder="Post Title"
-                            />
+                            <div style={{display: 'flex', flexDirection: 'column'}}>
+                                <form onSubmit={this.submit}>
+                                    <label>Title:</label>
+                                    <input
+                                        type="text"
+                                        ref={input => this.title = input}
+                                        placeholder="Title"
+                                    />
+                                    <label>Content:</label>
 
+                                    <input
+                                        type="text"
+                                        ref={input => this.content = input}
+                                        placeholder="Content"
+                                    />
+                                    <label>Type:</label>
+
+                                    <input
+                                        type="text"
+                                        ref={input => this.type = input}
+                                        placeholder="Type"
+                                    />
+                                    <label>Author:</label>
+
+                                    <input
+                                        type="text"
+                                        ref={input => this.author = input}
+                                        placeholder="Author"
+                                    />
+                                    {/* should remove this */}
+                                    <label>User Id:</label>
+
+                                    <input
+                                        type="text"
+                                        ref={input => this.id = input}
+                                        placeholder="Id"
+                                    />
+                                    <button type="submit">Submit</button>
+                                </form>
                             </div>
-                        </div>
-                   
+                    </div>
 
-                        </div>
-                       
-
-                        </div>
             </div>
         );
     }
