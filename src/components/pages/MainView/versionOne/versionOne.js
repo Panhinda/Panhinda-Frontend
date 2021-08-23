b5fe52cec1b4761a3868e72ee958458fc1aea6e3
kanchana-sankalpa <mdksankalpa@gmail.com>
2021-08-23 16:31:46 +0930

    mapped version 2 to home screnn

--- src/components/pages/MainView/versionOne/versionOne.js
+++ src/components/pages/MainView/versionOne/versionOne.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Version1 from "./../../textgenrnn/textgenrnn";
+import Version2 from "./../../tpu/tpu";
 
 import Header from "./../Nav/nav";
 
@@ -8,7 +9,7 @@ function LandingPage() {
     <div className="app-container">
       <Header />
       <div className="banner">
-        <Version1 />
+        <Version2 />
       </div>
     </div>
   );
