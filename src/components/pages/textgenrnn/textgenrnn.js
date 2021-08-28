00e0a344f188162549df0b4d3a428faaa9f629dd
https://github.com/yasashit <64302398+yasashit@users.noreply.github.com>
2021-08-28 18:08:04 +0530

    responsicve added for mobile /
    text color changed

--- src/components/pages/textgenrnn/textgenrnn.js
+++ src/components/pages/textgenrnn/textgenrnn.js
@@ -45,55 +45,60 @@ class textgenrnn extends Component {
           <div className="banner-shade">
             <div className="container">
               <div className="row">
-                <div className="col-12 col-sm-12 main-div">
-                  <div style={{ width: "33%" }}>
-                    <div style={{ width: "1190px", paddingLeft: "70px" }}>
-                      <h3>සිංහල පද ගලපමු-පලමු පිටපත</h3>
-                    </div>
-                    <div>
-                      <div
-                        style={{ wordBreak: "break-all" }}
-                        className="layer3-cover"
-                      >
-                       පළමු පිටපත සදහා සහය :
-                       <br/> fushenkao - https://github.com/fushenkao/Sinhala-Lyrics-Gen ,
-                       <br/> minimaxir - https://github.com/minimaxir/textgenrnn 
-                       <br/> පසුතලය : කැටපත් පවුර, සීගිරිය (Sigiri Mirror Wall) 
-                       <br/>
-                       http://thenationaltrust.lk/wp-content/uploads/2018/06/nds-nt-sigiriya.pdf
-                       <br/>වෙනස්කම් වල ඉතිහාසය : 
-                       <br/>https://ksankalpa.blogspot.com/2021/02/panhidalyrics-chages-log.html</div>
-                    </div>
-                  </div>
-                  <div className="second-side" style={{ width: "63%" }}>
-                    <div>
+                <div className="col col-sm-12 main-head txtcolor">
+                  <h3>සිංහල පද ගලපමු-පලමු පිටපත</h3>
+                </div>
+              </div>{" "}
+              <div className="row row-details">
+                <div className="col-xl-5 col-lg-4 layer3-cover txtcolor">
+                  පළමු පිටපත සදහා සහය :
+                  <br /> fushenkao -
+                  https://github.com/fushenkao/Sinhala-Lyrics-Gen ,
+                  <br /> minimaxir - https://github.com/minimaxir/textgenrnn
+                  <br /> පසුතලය : කැටපත් පවුර, සීගිරිය (Sigiri Mirror Wall)
+                  <br />
+                  http://thenationaltrust.lk/wp-content/uploads/2018/06/nds-nt-sigiriya.pdf
+                  <br />
+                  වෙනස්කම් වල ඉතිහාසය :
+                  <br />
+                  https://ksankalpa.blogspot.com/2021/02/panhidalyrics-chages-log.html
+                </div>
+
+                <div className="col-xl-7 col-lg-8   second-side txtcolor">
+                  <div className="row">
+                    <div className="col-12 col-sm-8">
                       <input
+                        className="txtcolor"
                         className="user_input"
                         type="text"
                         ref={(input) => (this.inputtext = input)}
                         placeholder="කැමති ආරම්භක වචන(ය)"
                       />
+                    </div>
+                    <div className="col-12 col-sm-4 txtcolor ">
                       <button
-                        className="button"
+                        className=" button-one"
                         onClick={this.submit}
                         type="submit"
                       >
-                        <span>පද ගැළපීම </span>
+                        <span className="txtcolor">පද ගැළපීම </span>
                       </button>
                     </div>
+                  </div>
 
-                    <div>
-                      <textarea
-                        type="text"
-                        ref={(input) => (this.content = input)}
-                        placeholder="යෝජිත පද..."
-                      />
-                    </div>
+                  <div className="col-12 t-area txtcolor">
+                    <textarea
+                      className="txtcolor"
+                      type="text"
+                      ref={(input) => (this.content = input)}
+                      placeholder="යෝජිත පද..."
+                    />
                   </div>
                 </div>
-                <Footer />
               </div>
             </div>
+
+            <Footer />
           </div>
         </div>
       </div>
